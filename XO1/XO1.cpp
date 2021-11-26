e9851c5b64f6c1aa46f64ff8b97cfd913313ee71
Opra <Opra@DESKTOP-HCEI8NH>
2021-11-26 21:13:24 +0300

    Добавьте файлы проекта.

---
+++ XO1/XO1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main()
+ {
+    char array [9] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+    int player_one = 0;
+    int player_two = 1;
+    printf(array);
+    printf("\n");
+
+    for (int i = 1; i <= 9; i++)
+        if (i % 2 > 0) {
+            printf("Write X = ");
+            scanf_s("%d", &player_one);
+            array[player_one] = 'X';
+            printf(array);
+            printf("\n");
+        }
+        else {
+            printf("Write O = ");
+            scanf_s("%d", &player_two);
+            array[player_two] = 'O';
+            printf(array);
+            printf("\n");
+            }
+
+    return 0;
+}
